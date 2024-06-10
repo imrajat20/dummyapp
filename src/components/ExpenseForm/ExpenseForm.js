d53035a3bbb050f51c208c93314c00320bb3d859
imrajat20 <yaduvanshiamit3@gmail.com>
2024-06-10 08:24:54 +0530

    eight

--- src/components/ExpenseForm/ExpenseForm.js
+++ src/components/ExpenseForm/ExpenseForm.js
@@ -1,5 +1,6 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import classes from './ExpenseForm.module.css';
+import axios from "axios";
 
 const ExpenseForm = () => {
   const [cost, setCost] = useState('');
@@ -7,10 +8,34 @@ const ExpenseForm = () => {
   const [category, setCategory] = useState('');
   const [expenses, setExpenses] = useState([]);
 
-  const submitHandler = (e) => {
+  useEffect(() => {
+    const fetchExpenses = async () => {
+      try {
+        const response = await axios.get('https://ecommerce-web-c8b78-default-rtdb.firebaseio.com/expenses.json');
+        console.log(response);
+        const data = response.data;
+        const loadedExpenses = data ? Object.values(data) : [];
+        setExpenses(loadedExpenses);
+      } catch (error) {
+        console.error("Error fetching data: ", error);
+      }
+    };
+
+    fetchExpenses();
+  }, []);
+
+  const submitHandler = async (e) => {
     e.preventDefault();
     const newExpense = { cost, description, category };
-    setExpenses([...expenses, newExpense]);
+  
+    try {
+      await axios.post('https://ecommerce-web-c8b78-default-rtdb.firebaseio.com/expenses.json', newExpense);
+      setExpenses([...expenses, newExpense]);
+    } catch (error) {
+      console.error("Error adding data: ", error);
+    }
+    
+
     setCost('');
     setDescription('');
     setCategory('');
@@ -42,7 +67,7 @@ const ExpenseForm = () => {
           <ul>
             {expenses.map((expense, index) => (
               <li key={index}>
-                <span>Cost: {expense.cost}</span> <br/> 
+                <span>Cost: {expense.cost}$</span> <br/> 
                 <span>Description: {expense.description}</span> <br/> 
                 <span>Category: {expense.category}</span>
               </li>
